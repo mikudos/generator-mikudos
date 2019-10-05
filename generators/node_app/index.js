885c38f13e5376f8ae7dcb1c1c22a335b0e0f893
qiushanyu <yue.guanyu@hotmail.com>
2019-10-05 10:10:32 +0800

    111

--- generators/node_app/index.js
+++ generators/node_app/index.js
@@ -1,5 +1,8 @@
 var Generator = require('yeoman-generator');
 var inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const mkdir = require('mkdirp');
 
 module.exports = class extends Generator {
     // The name `constructor` is important here
@@ -14,17 +17,19 @@ module.exports = class extends Generator {
         this.log('method 1 just ran');
     }
 
-    method2() {
-        this.log('method 2 just ran');
-    }
-
     async initializing() { }
     async prompting() {
         this.answers = await this.prompt([
             {
                 type: "input",
-                name: "name",
-                message: "Your Node project name",
+                name: "projectName",
+                message: "Your Golang project name",
+                default: this.appname // Default to current folder name
+            },
+            {
+                type: "input",
+                name: "serviceName",
+                message: "Your Golang micro service name",
                 default: this.appname // Default to current folder name
             },
             {
@@ -33,14 +38,66 @@ module.exports = class extends Generator {
                 message: "Would you like to enable the Cool feature?"
             }
         ]);
+        this.answers.projectName = this.answers["projectName"].replace(/[A-Z]/, word => `_${word.toLowerCase()}`).replace(/\s+/g, '_').toLowerCase();
+        this.answers.serviceName = this.answers["serviceName"].replace(/[A-Z]/, word => `_${word.toLowerCase()}`).replace(/\s+/g, '_').toLowerCase();
+        let repoUrl = await this.prompt([
+            {
+                type: 'input',
+                name: 'repoUrl',
+                message: 'What is your repository URL?',
+                default: `github.com/${this.answers["projectName"]}/${this.answers["serviceName"]}`
+            }
+        ])
+        this.answers.repoUrl = repoUrl["repoUrl"].replace(/^https:\/\//, '').toLowerCase();
     }
     async configuring() { }
     async default() { }
     async writing() {
-        this.log("app name", this.answers.name);
+        this.log("app serviceName", this.answers.serviceName);
+        this.log("app repoUrl", this.answers.repoUrl);
         this.log("cool feature", this.answers.cool);
+        let dirs = {}
+        dirs.configsDir = 'config';
+        dirs.brokerDir = 'broker';
+        dirs.clientsDir = 'clients';
+        dirs.deploymentDir = 'deployment';
+        dirs.servicesDir = 'services';
+        dirs.modelsDir = 'models';
+        var configObj = {
+            appName: this.answers.projectName,
+            serviceName: this.answers.serviceName,
+            repoUrl: this.answers.repoUrl
+        }
+        for (const key in dirs) {
+            if (dirs.hasOwnProperty(key)) {
+                const element = dirs[key];
+                mkdir.sync(this.destinationPath(element));
+                let files = fs.readdirSync(this.templatePath(element))
+                this.log("element:", this.templatePath(element))
+                this.log("files:", files)
+            }
+        }
+        var rootFiles = ['.gitignore', '.dockerignore', 'Dockerfile', 'crons.yaml', 'LICENSE', 'update_proto.sh']
+        var rootTemplate = ['Makefile', 'README.md', '_main.go', '_go.mod']
+        // rootFiles.map(fname => {
+        //     this.fs.copy(
+        //         this.templatePath(fname),
+        //         path.join("./", fname)
+        //     )
+        // })
+        // rootTemplate.map(fname => {
+        //     let fName = fname.replace(/^_/, "")
+        //     this.fs.copyTpl(
+        //         this.templatePath(fname),
+        //         path.join("./", fName),
+        //         configObj
+        //     )
+        // })
     }
     async conflicts() { }
     async install() { }
-    async end() { }
+    async end() {
+        // add exicute right to the bash file
+        fs.chmodSync(path.join("./", 'update_proto.sh'), 755)
+    }
 };
