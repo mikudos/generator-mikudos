561f8d6ae8c716a9489e6f06954e62923aea4e89
guanyu <yue.guanyu@hotmail.com>
2019-10-29 14:42:23 +0800

    fix mongoose error

--- generators/node_app/templates/_mongoose.js
+++ generators/node_app/templates/_mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-    mongoose.connect(app.config.get('mongodb'), { useNewUrlParser: true });
+    mongoose.connect(app.config.get('mongodb'), { useUnifiedTopology: true, useNewUrlParser: true });
     mongoose.Promise = global.Promise;
     app.mongooseClient = mongoose;
     app.context.models = {};
