7b80fd2772473282a3b1ba3d172456f05310c843
guanyu <yue.guanyu@hotmail.com>
2019-11-15 01:52:30 +0800

    update template

--- generators/ts_app/templates/src/services/index.ts
+++ generators/ts_app/templates/src/services/index.ts
@@ -1,6 +1,3 @@
 import { Application } from 'mikudos-node-app';
-import method from './_method';
 
-export default function(app: Application): void {
-    app.configure(method);
-}
+export default function(app: Application): void {}
