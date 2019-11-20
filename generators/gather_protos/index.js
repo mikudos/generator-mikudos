e4b7f1b7a03b2193bb0645df110ea4a03804ac77
guanyu <yue.guanyu@hotmail.com>
2019-11-21 02:18:06 +0800

    update to gather protos

--- generators/gather_protos/index.js
+++ generators/gather_protos/index.js
@@ -1,6 +1,8 @@
 var Generator = require('yeoman-generator');
 const fs = require('fs');
+const yaml = require('js-yaml');
 const _ = require('lodash');
+const { ProtoInfo } = require('../../transform/proto');
 
 module.exports = class extends Generator {
     // The name `constructor` is important here
@@ -11,6 +13,7 @@ module.exports = class extends Generator {
     }
 
     async initializing() {
+        this.protoInfos = {}
         // list all proto names
         if (!this.options["protoList"]) {
             this.protos = fs.readdirSync(this.destinationPath("./proto"))
@@ -18,13 +21,36 @@ module.exports = class extends Generator {
             this.log("the following proto list will be include to be scanning: \n", this.protos.join("\n"))
         }
     }
-    async prompting() { }
+    async prompting() {
+        let format = await this.prompt({
+            type: "list",
+            name: "format",
+            message: "Select the format that you want to generate to:",
+            choices: [{ name: "YAML" }, { name: "JSON" }]
+        })
+        this.format = format.format
+    }
     async configuring() {
         // scanning all protos
-        
+        for (const proto of this.protos) {
+            let tempProtoInfo = await new ProtoInfo(`./proto/${proto}/${proto}.proto`).init();
+            for (const key in tempProtoInfo.methodsList) {
+                let list = tempProtoInfo.methodsList[key];
+                tempProtoInfo.methodsList[key] = list.map(method => {
+                    return { file: proto, package: tempProtoInfo.packageName, service: tempProtoInfo.serviceList[key], path: `${tempProtoInfo.packageName}.${tempProtoInfo.serviceList[key]}.${method.name}`, ...method }
+                })
+            }
+            this.protoInfos[proto] = tempProtoInfo.methodsList;
+        }
     }
     async default() { }
-    async writing() { }
+    async writing() {
+        if (this.format === "YAML") {
+            this.fs.write(this.destinationPath("./proto/proto_info.yml"), yaml.safeDump(this.protoInfos))
+        } else if (this.format === "JSON") {
+            this.fs.writeJSON(this.destinationPath("./proto/proto_info.json"), this.protoInfos)
+        }
+    }
     async conflicts() { }
     async install() { }
     async end() { }
