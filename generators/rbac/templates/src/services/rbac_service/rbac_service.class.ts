c19c350d4800586b337f0e393a24c2faa914bdef
guanyu <yue.guanyu@hotmail.com>
2019-11-29 01:32:59 +0800

    add superUser and defaultUserGroup

--- generators/rbac/templates/src/services/rbac_service/rbac_service.class.ts
+++ generators/rbac/templates/src/services/rbac_service/rbac_service.class.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { Application } from 'mikudos-node-app';
 
 export default class {
     options: any;
@@ -6,12 +7,33 @@ export default class {
         this.options = options || {};
     }
 
+    private checkDefaultRoleRight(
+        fullPathMethod: string,
+        expStr: string
+    ): boolean {
+        let reg = new RegExp(expStr, 'i');
+        let matched = fullPathMethod.match(reg);
+        return !!matched && matched[0] === fullPathMethod;
+    }
+
     async VerifyAccessRightWithRids(ctx: any) {
+        const roleIds = ctx.req.ids;
         let method = ctx.req.fullPathMethod;
+        if ((roleIds as [number]).includes(-1)) {
+            // 超级角色
+            return (ctx.res = { passed: true });
+        } else if ((roleIds as [number]).includes(0)) {
+            // 默认角色
+            return (ctx.res = {
+                passed: this.checkDefaultRoleRight(
+                    method,
+                    (ctx.app as Application).config.default_method_regex || '.*'
+                )
+            });
+        }
         let pathArr = (method as string).split('.');
         let first = _.dropRight(pathArr, 2).join('.');
         pathArr = _.reverse(pathArr);
-        const roleIds = ctx.req.ids;
         const sequelizeClient = ctx.app.get('sequelizeClient');
         method = (method as string).replace(/\*/g, '%');
         let res = await sequelizeClient.query(
@@ -34,13 +56,24 @@ export default class {
         ctx.res = { passed: !!res[0].matched };
     }
 
-    // TODO:
     async VerifyAccessRightWithGids(ctx: any) {
+        const groupIds = ctx.req.ids;
         let method = ctx.req.fullPathMethod;
+        if ((groupIds as [number]).includes(-1)) {
+            // 超级用户
+            return (ctx.res = { passed: true });
+        } else if ((groupIds as [number]).includes(0)) {
+            // 默认用户分组
+            return (ctx.res = {
+                passed: this.checkDefaultRoleRight(
+                    method,
+                    (ctx.app as Application).config.default_method_regex || '.*'
+                )
+            });
+        }
         let pathArr = (method as string).split('.');
         let first = _.dropRight(pathArr, 2).join('.');
         pathArr = _.reverse(pathArr);
-        const groupIds = ctx.req.ids;
         const sequelizeClient = ctx.app.get('sequelizeClient');
         let res = await sequelizeClient.query(
             `SELECT COUNT(*) as matched FROM \`role_methods\` INNER JOIN \`groups\` WHERE role_methods.roleId IN (groups.rids) AND groups.id IN (${groupIds.toString()}) AND (role_methods.value = '*' OR role_methods.value = '*.*' OR role_methods.value = '*.*.*' OR role_methods.value = '${first}.*' OR role_methods.value = '${first}.*.*' OR role_methods.value = '${first}.${
